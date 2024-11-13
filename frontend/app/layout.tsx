87330b2de060a4819fd95cf4c78749ad997cfb42
Hugo Lopes <hugo.rodrigue.lopes@gmail.com>
2024-11-13 22:41:22 +0000

    initial commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react'
+
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
+
+export const metadata: Metadata = {
+  title: 'StoreIt',
+  description: 'StoreIt - The only storage solution you will need',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} font-poppins antialiased`}>
+        {children}
+      </body>
+    </html>
+  )
+}
